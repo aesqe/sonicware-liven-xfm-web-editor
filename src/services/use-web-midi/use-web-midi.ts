2c71021fa02dd1e648aa829da328b7e063112909
Bruno Babic <aesqe@skyphe.org>
2025-04-07 22:49:59 +0200

    Move sysex listener to useWebMidi hook

--- src/services/use-web-midi/use-web-midi.ts
+++ src/services/use-web-midi/use-web-midi.ts
@@ -1,13 +1,24 @@
 import { useEffect, useRef } from 'react'
-import { useSetAtom } from 'jotai'
-import { WebMidi } from 'webmidi'
+import { useSetAtom, useAtomValue } from 'jotai'
+import { WebMidi, MessageEvent } from 'webmidi'
 
-import { midiInputListAtom, midiOutputListAtom, webMidiEnabledAtom } from '../../store/atoms'
+import {
+  logSysExAtom,
+  midiInputAtom,
+  midiInputListAtom,
+  midiOutputListAtom,
+  webMidiEnabledAtom
+} from '../../store/atoms'
+import { convert78 } from '../convert78/convert78'
+import { decode8bit } from '../decode8bit/decode8bit'
+import { XFMPatch } from '../../types'
 
-export const useWebMidi = () => {
+export const useWebMidi = (handlePatchChange: (patch: XFMPatch) => void) => {
   const setMidiInputList = useSetAtom(midiInputListAtom)
   const setMidiOutputList = useSetAtom(midiOutputListAtom)
   const setWebMidiEnabled = useSetAtom(webMidiEnabledAtom)
+  const midiInput = useAtomValue(midiInputAtom)
+  const logSysEx = useAtomValue(logSysExAtom)
   const listenersAdded = useRef(false)
 
   useEffect(() => {
@@ -40,4 +51,24 @@ export const useWebMidi = () => {
       WebMidi.removeListener('disconnected', updateMidiPorts)
     }
   }, [setMidiInputList, setMidiOutputList, setWebMidiEnabled])
+
+  useEffect(() => {
+    midiInput?.addListener('sysex', (e: MessageEvent) => {
+      if (e.message.data.length > 20) {
+        const decodedPatch = decode8bit(convert78(e.message.data))
+
+        if (logSysEx) {
+          console.log('Raw SysEx:', new Uint8Array(e.message.data))
+          console.log('Decoded SysEx:', new Uint8Array(convert78(e.message.data)))
+          console.log('Patch:', decodedPatch)
+        }
+
+        handlePatchChange(decodedPatch)
+      }
+    })
+
+    return () => {
+      midiInput?.removeListener('sysex')
+    }
+  }, [handlePatchChange, logSysEx, midiInput])
 }
