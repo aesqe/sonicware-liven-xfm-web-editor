1ee02cb888c4d2e7308234dca10d348ffe9bd3e5
Bruno Babic <aesqe@skyphe.org>
2025-04-07 22:51:02 +0200

    Add globalRefs atom

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -2,7 +2,7 @@ import { atom } from 'jotai'
 import { Input, Output } from 'webmidi'
 
 import initPatch from '../assets/presets/initpatch.json'
-import { XFMPatch, ADSRValues, OperatorValues, RandomizationOptions } from '../types'
+import { XFMPatch, ADSRValues, OperatorValues, RandomizationOptions, GlobalRefs } from '../types'
 
 export const webMidiEnabledAtom = atom<boolean>(false)
 export const midiInputAtom = atom<Input | null>(null)
@@ -21,10 +21,19 @@ export const randomizationOptionsAtom = atom<RandomizationOptions>({
 })
 
 export const messagesDelayAtom = atom<number>(16.7)
-export const sysexSendThrottleTimeAtom = atom<number>(200)
+export const sysexSendThrottleTimeAtom = atom<number>(250)
 
 export const patchClipboardAtom = atom<XFMPatch | null>(null)
 export const envelopeClipboardAtom = atom<ADSRValues | null>(null)
 export const operatorClipboardAtom = atom<OperatorValues | null>(null)
 
 export const logSysExAtom = atom<boolean>(false)
+
+export const globalRefsAtom = atom<GlobalRefs>({
+  op1Ref: undefined,
+  op2Ref: undefined,
+  op3Ref: undefined,
+  op4Ref: undefined,
+  pitchAdsrRef: undefined,
+  patchNameRef: undefined
+})
